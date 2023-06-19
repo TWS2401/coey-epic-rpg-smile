c42275ba5cd6bd923c54b031945bdf51e4378c6a
TWS2401 <joshuabetito2401@gmail.com>
2023-06-19 22:15:16 +0100

    actually fix joystick???

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1687124961|93166103";
+const CACHE_VERSION = "1687209088|324661844";
 const CACHE_PREFIX = "UNDERTALE Engine-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
